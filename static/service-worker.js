1ab6bd4d8c9a7a832eca6cd3761a9bf10da63c06
seuiaaaaaaaa <207196368+seuiaaaaaaaa@users.noreply.github.com>
2026-05-03 23:30:33 +0800

    Fix tablet dashboard card layout

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "hormonacare-static-v25";
-const STATIC_VERSION = "20260503-dashboardfallback";
+const CACHE_NAME = "hormonacare-static-v26";
+const STATIC_VERSION = "20260503-tabletcards";
 const STATIC_ASSETS = [
     `/static/css/style.css?v=${STATIC_VERSION}`,
     `/static/js/app.js?v=${STATIC_VERSION}`,
