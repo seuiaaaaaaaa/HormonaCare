87ef6d47ac905b1eaed3b1ebe2bc9b563eeae8c9
seuiaaaaaaaa <207196368+seuiaaaaaaaa@users.noreply.github.com>
2026-05-03 22:44:35 +0800

    Improve responsive layout

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "hormonacare-static-v19";
-const STATIC_VERSION = "20260503-authintro";
+const CACHE_NAME = "hormonacare-static-v20";
+const STATIC_VERSION = "20260503-responsive";
 const STATIC_ASSETS = [
     `/static/css/style.css?v=${STATIC_VERSION}`,
     `/static/js/app.js?v=${STATIC_VERSION}`,
