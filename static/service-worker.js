a9d8b8b97acfef11377d8ff53a810836c061b96d
seuiaaaaaaaa <207196368+seuiaaaaaaaa@users.noreply.github.com>
2026-05-03 22:20:17 +0800

    Make mobile nav horizontally scrollable

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "hormonacare-static-v17";
-const STATIC_VERSION = "20260503-mobiledoctors";
+const CACHE_NAME = "hormonacare-static-v18";
+const STATIC_VERSION = "20260503-mobiledoctorsscroll";
 const STATIC_ASSETS = [
     `/static/css/style.css?v=${STATIC_VERSION}`,
     `/static/js/app.js?v=${STATIC_VERSION}`,
