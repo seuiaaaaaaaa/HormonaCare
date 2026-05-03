4bfabcb6cd4f5d6d07316d7c4ccd2e58233a1a95
seuiaaaaaaaa <207196368+seuiaaaaaaaa@users.noreply.github.com>
2026-05-03 23:01:33 +0800

    Fix tablet navigation layout

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "hormonacare-static-v21";
-const STATIC_VERSION = "20260503-layoutfix";
+const CACHE_NAME = "hormonacare-static-v22";
+const STATIC_VERSION = "20260503-tabletnav";
 const STATIC_ASSETS = [
     `/static/css/style.css?v=${STATIC_VERSION}`,
     `/static/js/app.js?v=${STATIC_VERSION}`,
