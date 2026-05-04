fccb5251589320437fb4e0688e80bd5cbd0182c6
seuiaaaaaaaa <207196368+seuiaaaaaaaa@users.noreply.github.com>
2026-05-04 10:59:15 +0800

    Use bottom scroll navigation on tablet

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "hormonacare-static-v26";
-const STATIC_VERSION = "20260503-tabletcards";
+const CACHE_NAME = "hormonacare-static-v27";
+const STATIC_VERSION = "20260504-tabletbottomnav";
 const STATIC_ASSETS = [
     `/static/css/style.css?v=${STATIC_VERSION}`,
     `/static/js/app.js?v=${STATIC_VERSION}`,
