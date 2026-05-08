00bdcd741807222a943188cdf5cab67c24a706d4
seuiaaaaaaaa <207196368+seuiaaaaaaaa@users.noreply.github.com>
2026-05-08 22:54:41 +0800

    Simplify medication status actions

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "hormonacare-static-v40";
-const STATIC_VERSION = "20260508-meds-card-fix";
+const CACHE_NAME = "hormonacare-static-v41";
+const STATIC_VERSION = "20260508-meds-clear-today";
 const STATIC_ASSETS = [
     `/static/css/style.css?v=${STATIC_VERSION}`,
     `/static/js/app.js?v=${STATIC_VERSION}`,
